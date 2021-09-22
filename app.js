dc46302dc096075d6047f7471bd3e7e45055b2cb
Fourswords <58376522+fourswordsio@users.noreply.github.com>
2021-09-22 12:36:09 -0400

    create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+const createRequest = require('./index').createRequest;
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT ||  85;
+
+app.use(bodyParser.json());
+
+app.post('/', (req, res) => {
+  console.log('POST Data: ', req.body);
+  createRequest(req.body, (status, result) => {
+    console.log('Result: ', result);
+    res.status(status).json(result);
+  });
+});
+
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
